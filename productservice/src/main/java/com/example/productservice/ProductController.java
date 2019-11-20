970f1ba65816238998b30ca89cf9cd51ec0d4711
davidsackstein <davids@codeprecise.com>
2019-11-20 09:38:57 +0200

    Correct implementation of project dependency

--- productservice/src/main/java/com/example/productservice/ProductController.java
+++ productservice/src/main/java/com/example/productservice/ProductController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
-
     @GetMapping(value = "helloworld")
     public String GetHelloWorld() {
         return ProductFactory.createProduct();
